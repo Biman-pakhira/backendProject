78daece8dbd03646e9958dc7a8a153c49f0b598c
Biman Pakhira <bimanpakhira2@gmail.com>
2026-02-03 12:44:50 +0530

    Updated all API for now 2

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 require('dotenv').config()
 const express = require('express')
-const userModel = require('../model/note.model')
-
+const userModel = require(`../model/note.model`)
 
 const app = express()
 app.use(express.json())
 
 app.post("/notes", async (req, res) => {
     const data = req.body
-    
     try {
        await userModel.create({
             name: data.name,
@@ -29,6 +27,36 @@ app.get("/notes", async (req, res) => {
             message: " Data Fetched Successfully",
             notes: data
          })
+    } catch (error) {
+        console.log(error);
+    }
+})
+app.delete("/notes/:id", async (req, res) => {
+    try {  
+        const id = req.params.id;
+        await userModel.findByIdAndDelete({
+            _id: id
+      })
+      res.status(201).json({
+          message:"Data Deleted Successfully"
+    })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.patch("/notes/:id", async (req, res) => {
+    try {  
+        const id = req.params.id;
+        const job = req.body.job
+        await userModel.findByIdAndUpdate({
+            _id: id
+      },{
+            job: job
+      })
+      res.status(201).json({
+          message:"Data Deleted Successfully"
+    })
     } catch (error) {
         console.log(error);
     }
