dc7220306f9fa82f8828c7dc5f77935aa86dba8f
Biman Pakhira <bimanpakhira2@gmail.com>
2026-02-03 05:43:54 +0530

    Dotend Added

--- db/db.js
+++ db/db.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+
 
 async function connectDB() {
     try {
-        await mongoose.connect("mongodb+srv://Guddu:5E18C6wAEfT6WHkR@cluster0.y7sbyo6.mongodb.net/")
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Database Connected Succesfully");
         
         
