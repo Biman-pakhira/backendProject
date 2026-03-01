a7da491e6dab7d2a4d5d50e2be9b0a83cc545c7f
Biman Pakhira <bimanpakhira2@gmail.com>
2026-03-01 11:15:54 +0530

    chore: remove node_modules files.

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/app")
-const connectDB = require("./db/db")
-
-connectDB();
-const PORT = 8000;
-app.listen(PORT, ()=>{
-    console.log(`Server Has Been Created successfully on PORT:${PORT}`);
-    
-})
